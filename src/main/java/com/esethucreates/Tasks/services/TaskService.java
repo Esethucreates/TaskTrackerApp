0414cfb17a11f397d1a9ab35e45c222f10d9b0a7
EsethCreatesLife <esethutradewarr43@gmail.com>
2025-02-17 16:15:25 +0200

    Created service interface for task and tasklist

---
+++ src/main/java/com/esethucreates/Tasks/services/TaskService.java
@@ -0,0 +1,20 @@
+package com.esethucreates.Tasks.services;
+
+import com.esethucreates.Tasks.domain.entities.Task;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface TaskService {
+    List<Task> listTasks(UUID taskListId);
+
+    Task createTask(UUID taskListId, Task task);
+
+    Optional<Task> getTask(UUID taskListId, UUID taskId);
+
+    Task updateTask(UUID taskListId, UUID taskId, Task task);
+
+    void delete(UUID taskListId, UUID taskId);
+
+}
